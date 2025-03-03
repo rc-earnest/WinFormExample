38c47d8485f12de94a60eac5ca9a90fcc94482e0
rc-earnest <rc.earnest04@gmail.com>
2025-03-03 08:24:52 -0700

    add header and options

--- WinFormExample/winFormExampleForm.Designer.vb
+++ WinFormExample/winFormExampleForm.Designer.vb
@@ -22,10 +22,17 @@ Partial Class winFormExampleForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.SuspendLayout()
+        '
+        'winFormExampleForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(818, 486)
+        Me.Name = "winFormExampleForm"
+        Me.Text = "windowFormExample"
+        Me.ResumeLayout(False)
+
     End Sub
 
 End Class
--- WinFormExample/winFormExampleForm.vb
+++ WinFormExample/winFormExampleForm.vb
@@ -1,3 +1,19 @@
-﻿Public Class winFormExampleForm
+﻿'Rudy Earnest
+'RCET 2265 
+'Form Examples
+'git link
+
+Option Strict On
+Option Explicit On
+Public Class winFormExampleForm
+
+
+
+
+
+
+
+
+
 
 End Class
