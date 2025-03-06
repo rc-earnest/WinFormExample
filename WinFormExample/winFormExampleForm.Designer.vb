5fc122cef667678b5e2019ff8b69bca801d13556
rc-earnest <rc.earnest04@gmail.com>
2025-03-06 08:20:14 -0700

    yes

--- WinFormExample/winFormExampleForm.Designer.vb
+++ WinFormExample/winFormExampleForm.Designer.vb
@@ -30,6 +30,17 @@ Partial Class winFormExampleForm
         Me.Label1 = New System.Windows.Forms.Label()
         Me.AgeTextBox = New System.Windows.Forms.TextBox()
         Me.Label2 = New System.Windows.Forms.Label()
+        Me.UpperRadioButton = New System.Windows.Forms.RadioButton()
+        Me.LowerRadioButton = New System.Windows.Forms.RadioButton()
+        Me.FirstLastRadioButton = New System.Windows.Forms.RadioButton()
+        Me.LastFirstRadioButton = New System.Windows.Forms.RadioButton()
+        Me.CaseGroupBox = New System.Windows.Forms.GroupBox()
+        Me.FormatGroupBox = New System.Windows.Forms.GroupBox()
+        Me.ReverseCheckBox = New System.Windows.Forms.CheckBox()
+        Me.WhiteSpaceCheckBox = New System.Windows.Forms.CheckBox()
+        Me.RandomCheckBox = New System.Windows.Forms.CheckBox()
+        Me.CaseGroupBox.SuspendLayout()
+        Me.FormatGroupBox.SuspendLayout()
         Me.SuspendLayout()
         '
         'ExitButton
@@ -37,7 +48,7 @@ Partial Class winFormExampleForm
         Me.ExitButton.Location = New System.Drawing.Point(731, 451)
         Me.ExitButton.Name = "ExitButton"
         Me.ExitButton.Size = New System.Drawing.Size(75, 23)
-        Me.ExitButton.TabIndex = 4
+        Me.ExitButton.TabIndex = 11
         Me.ExitButton.Text = "E&xit"
         Me.ExitButton.UseVisualStyleBackColor = True
         '
@@ -46,7 +57,7 @@ Partial Class winFormExampleForm
         Me.UpdateButton.Location = New System.Drawing.Point(638, 451)
         Me.UpdateButton.Name = "UpdateButton"
         Me.UpdateButton.Size = New System.Drawing.Size(75, 23)
-        Me.UpdateButton.TabIndex = 3
+        Me.UpdateButton.TabIndex = 10
         Me.UpdateButton.Text = "&Update"
         Me.UpdateButton.UseVisualStyleBackColor = True
         '
@@ -71,7 +82,7 @@ Partial Class winFormExampleForm
         Me.LastTextBox.Location = New System.Drawing.Point(124, 97)
         Me.LastTextBox.Name = "LastTextBox"
         Me.LastTextBox.Size = New System.Drawing.Size(210, 22)
-        Me.LastTextBox.TabIndex = 1
+        Me.LastTextBox.TabIndex = 2
         '
         'Label1
         '
@@ -87,7 +98,7 @@ Partial Class winFormExampleForm
         Me.AgeTextBox.Location = New System.Drawing.Point(124, 125)
         Me.AgeTextBox.Name = "AgeTextBox"
         Me.AgeTextBox.Size = New System.Drawing.Size(54, 22)
-        Me.AgeTextBox.TabIndex = 2
+        Me.AgeTextBox.TabIndex = 4
         '
         'Label2
         '
@@ -98,11 +109,120 @@ Partial Class winFormExampleForm
         Me.Label2.TabIndex = 7
         Me.Label2.Text = "Age"
         '
+        'UpperRadioButton
+        '
+        Me.UpperRadioButton.AutoSize = True
+        Me.UpperRadioButton.Location = New System.Drawing.Point(5, 20)
+        Me.UpperRadioButton.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.UpperRadioButton.Name = "UpperRadioButton"
+        Me.UpperRadioButton.Size = New System.Drawing.Size(101, 20)
+        Me.UpperRadioButton.TabIndex = 5
+        Me.UpperRadioButton.TabStop = True
+        Me.UpperRadioButton.Text = "Upper Case"
+        Me.UpperRadioButton.UseVisualStyleBackColor = True
+        '
+        'LowerRadioButton
+        '
+        Me.LowerRadioButton.AutoSize = True
+        Me.LowerRadioButton.Location = New System.Drawing.Point(5, 44)
+        Me.LowerRadioButton.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.LowerRadioButton.Name = "LowerRadioButton"
+        Me.LowerRadioButton.Size = New System.Drawing.Size(99, 20)
+        Me.LowerRadioButton.TabIndex = 6
+        Me.LowerRadioButton.TabStop = True
+        Me.LowerRadioButton.Text = "Lower Case"
+        Me.LowerRadioButton.UseVisualStyleBackColor = True
+        '
+        'FirstLastRadioButton
+        '
+        Me.FirstLastRadioButton.AutoSize = True
+        Me.FirstLastRadioButton.Location = New System.Drawing.Point(5, 18)
+        Me.FirstLastRadioButton.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.FirstLastRadioButton.Name = "FirstLastRadioButton"
+        Me.FirstLastRadioButton.Size = New System.Drawing.Size(84, 20)
+        Me.FirstLastRadioButton.TabIndex = 1
+        Me.FirstLastRadioButton.TabStop = True
+        Me.FirstLastRadioButton.Text = "First, Last"
+        Me.FirstLastRadioButton.UseVisualStyleBackColor = True
+        '
+        'LastFirstRadioButton
+        '
+        Me.LastFirstRadioButton.AutoSize = True
+        Me.LastFirstRadioButton.Location = New System.Drawing.Point(5, 46)
+        Me.LastFirstRadioButton.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.LastFirstRadioButton.Name = "LastFirstRadioButton"
+        Me.LastFirstRadioButton.Size = New System.Drawing.Size(84, 20)
+        Me.LastFirstRadioButton.TabIndex = 3
+        Me.LastFirstRadioButton.TabStop = True
+        Me.LastFirstRadioButton.Text = "Last, First"
+        Me.LastFirstRadioButton.UseVisualStyleBackColor = True
+        '
+        'CaseGroupBox
+        '
+        Me.CaseGroupBox.Controls.Add(Me.UpperRadioButton)
+        Me.CaseGroupBox.Controls.Add(Me.LowerRadioButton)
+        Me.CaseGroupBox.Location = New System.Drawing.Point(124, 151)
+        Me.CaseGroupBox.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.CaseGroupBox.Name = "CaseGroupBox"
+        Me.CaseGroupBox.Padding = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.CaseGroupBox.Size = New System.Drawing.Size(178, 80)
+        Me.CaseGroupBox.TabIndex = 12
+        Me.CaseGroupBox.TabStop = False
+        Me.CaseGroupBox.Text = "Case"
+        '
+        'FormatGroupBox
+        '
+        Me.FormatGroupBox.Controls.Add(Me.LastFirstRadioButton)
+        Me.FormatGroupBox.Controls.Add(Me.FirstLastRadioButton)
+        Me.FormatGroupBox.Location = New System.Drawing.Point(340, 54)
+        Me.FormatGroupBox.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.FormatGroupBox.Name = "FormatGroupBox"
+        Me.FormatGroupBox.Padding = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.FormatGroupBox.Size = New System.Drawing.Size(178, 80)
+        Me.FormatGroupBox.TabIndex = 13
+        Me.FormatGroupBox.TabStop = False
+        Me.FormatGroupBox.Text = "Format"
+        '
+        'ReverseCheckBox
+        '
+        Me.ReverseCheckBox.AutoSize = True
+        Me.ReverseCheckBox.Location = New System.Drawing.Point(129, 261)
+        Me.ReverseCheckBox.Name = "ReverseCheckBox"
+        Me.ReverseCheckBox.Size = New System.Drawing.Size(81, 20)
+        Me.ReverseCheckBox.TabIndex = 7
+        Me.ReverseCheckBox.Text = "Reverse"
+        Me.ReverseCheckBox.UseVisualStyleBackColor = True
+        '
+        'WhiteSpaceCheckBox
+        '
+        Me.WhiteSpaceCheckBox.AutoSize = True
+        Me.WhiteSpaceCheckBox.Location = New System.Drawing.Point(129, 288)
+        Me.WhiteSpaceCheckBox.Name = "WhiteSpaceCheckBox"
+        Me.WhiteSpaceCheckBox.Size = New System.Drawing.Size(106, 20)
+        Me.WhiteSpaceCheckBox.TabIndex = 8
+        Me.WhiteSpaceCheckBox.Text = "White Space"
+        Me.WhiteSpaceCheckBox.UseVisualStyleBackColor = True
+        '
+        'RandomCheckBox
+        '
+        Me.RandomCheckBox.AutoSize = True
+        Me.RandomCheckBox.Location = New System.Drawing.Point(129, 315)
+        Me.RandomCheckBox.Name = "RandomCheckBox"
+        Me.RandomCheckBox.Size = New System.Drawing.Size(81, 20)
+        Me.RandomCheckBox.TabIndex = 9
+        Me.RandomCheckBox.Text = "Random"
+        Me.RandomCheckBox.UseVisualStyleBackColor = True
+        '
         'winFormExampleForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(818, 486)
+        Me.Controls.Add(Me.RandomCheckBox)
+        Me.Controls.Add(Me.WhiteSpaceCheckBox)
+        Me.Controls.Add(Me.ReverseCheckBox)
+        Me.Controls.Add(Me.FormatGroupBox)
+        Me.Controls.Add(Me.CaseGroupBox)
         Me.Controls.Add(Me.AgeTextBox)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.LastTextBox)
@@ -113,6 +233,10 @@ Partial Class winFormExampleForm
         Me.Controls.Add(Me.ExitButton)
         Me.Name = "winFormExampleForm"
         Me.Text = "windowFormExample"
+        Me.CaseGroupBox.ResumeLayout(False)
+        Me.CaseGroupBox.PerformLayout()
+        Me.FormatGroupBox.ResumeLayout(False)
+        Me.FormatGroupBox.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -126,4 +250,13 @@ Partial Class winFormExampleForm
     Friend WithEvents Label1 As Label
     Friend WithEvents AgeTextBox As TextBox
     Friend WithEvents Label2 As Label
+    Friend WithEvents UpperRadioButton As RadioButton
+    Friend WithEvents LowerRadioButton As RadioButton
+    Friend WithEvents FirstLastRadioButton As RadioButton
+    Friend WithEvents LastFirstRadioButton As RadioButton
+    Friend WithEvents CaseGroupBox As GroupBox
+    Friend WithEvents FormatGroupBox As GroupBox
+    Friend WithEvents ReverseCheckBox As CheckBox
+    Friend WithEvents WhiteSpaceCheckBox As CheckBox
+    Friend WithEvents RandomCheckBox As CheckBox
 End Class
--- WinFormExample/winFormExampleForm.vb
+++ WinFormExample/winFormExampleForm.vb
@@ -6,17 +6,51 @@
 Option Strict On
 Option Explicit On
 Public Class winFormExampleForm
-    Sub Clear()
+    Sub SetDefaults()
+        TheFirstTextBox.Focus()
         TheFirstTextBox.Text = ""
         LastTextBox.Text = ""
         AgeTextBox.Text = ""
+        UpperRadioButton.Checked = True
+        LastFirstRadioButton.Checked = True
+        ReverseCheckBox.Checked = False
+        WhiteSpaceCheckBox.Checked = False
+        RandomCheckBox.Checked = False
     End Sub
 
+    Sub ReverseString()
+        If ReverseCheckBox.Checked = True Then
+            Me.Text = StrReverse(Me.Text)
+        End If
+    End Sub
 
+    Sub RemoveWhiteSpace()
+        If WhiteSpaceCheckBox.Checked Then
+            Me.Text = Replace(Me.Text, " ", "")
+        End If
+    End Sub
 
+    Sub SetCase()
+        If UpperRadioButton.Checked Then
+            TheFirstTextBox.Text = UCase(TheFirstTextBox.Text)
+            LastTextBox.Text = UCase(LastTextBox.Text)
 
+        ElseIf LowerRadioButton.Checked Then
+            TheFirstTextBox.Text = LCase(TheFirstTextBox.Text)
+            LastTextBox.Text = LCase(LastTextBox.Text)
 
+        End If
+    End Sub
 
+    Sub SetFormat()
+        If FirstLastRadioButton.Checked Then
+            Me.Text = (TheFirstTextBox.Text & ", " & LastTextBox.Text)
+
+        ElseIf LastFirstRadioButton.Checked Then
+            Me.Text = (LastTextBox.Text & ", " & TheFirstTextBox.Text)
+
+        End If
+    End Sub
 
 
     'Event Handlers *****************************************************************************************
@@ -24,11 +58,51 @@ Public Class winFormExampleForm
         Me.Close()
     End Sub
 
+    Function UserInputIsValid() As Boolean
+        Dim valid As Boolean = True
+        Dim message As String
+        If IsNumeric(AgeTextBox.Text) = False Then
+            valid = False
+            AgeTextBox.Focus()
+            message &= "Please enter a valid age" & vbNewLine
+        End If
+
+        If LastTextBox.Text = "" Then
+            valid = False
+            LastTextBox.Focus()
+            message &= "Last name is required" & vbNewLine
+        End If
+
+        If TheFirstTextBox.Text = "" Then
+            valid = False
+            TheFirstTextBox.Focus()
+            message &= "First name is required" & vbNewLine
+        End If
+
+        If Not valid Then
+            MsgBox(message, MsgBoxStyle.Exclamation, "User Input Fail!!!")
+        End If
+
+        Return valid
+    End Function
+
+
     Private Sub UpdateButton_Click(sender As Object, e As EventArgs) Handles UpdateButton.Click
         'Me.Text = "Forms Are Cool"
         ' Me.Text = StrReverse(Me.Text)
-        Me.Text = TheFirstTextBox.Text & " " & LastTextBox.Text
-        Clear()
+        If UserInputIsValid() Then
+
+            SetCase()
+            SetFormat()
+            ReverseString()
+            RemoveWhiteSpace()
+            SetDefaults()
+        End If
+
+    End Sub
+
+    Private Sub winFormExampleForm_Load(sender As Object, e As EventArgs) Handles Me.Load
+        SetDefaults()
     End Sub
 
 
